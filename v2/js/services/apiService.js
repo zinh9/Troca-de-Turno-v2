bffaba4b5e6b8dcef58e0e67317c1422a6e62c0e
Enzo Spadetto Rufino <127460831+zinh9@users.noreply.github.com>
2025-11-10 12:09:17 -0300

    Refactor API service methods for error handling

--- v2/js/services/apiService.js
+++ v2/js/services/apiService.js
@@ -22,45 +22,32 @@ export const api = {
             return { success: false, message: error.message };
         }
     },
+    /**
+     * 
+     * @param {FormData} 
+     * @returns {Promise<object>}
+     */
     postApresentacao: async (formData) => {
         try {
             const url = `${API_BASE_URL}post_apresentacao.asp`;
+
+            
     
             const response = await fetch(url, {
                 method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
                 body: formData
             });
     
-            if (!response.ok) {
-                throw new Error(`Erro HTTP: ${response.status}`);
+            if (!response) {
+                throw new Error(`Erro HTTP: ${response.message}`);
             }
     
             const data = await response.json();
-            console.log("Resposta da API:", data);
-    
-            if (data.success) {
-                // Sucesso na apresentação
-                // document.getElementById("mensagem").innerHTML = `
-                //     <div class='alert alert-success'>${data.message}</div>
-                // `;
-                setTimeout(() => {
-                    location.reload();
-                }, 5000);
-            } else {
-                // Tratamento de mensagens especiais
-                if (data.message.startsWith("confirmar|")) {
-                    const supervisaoOriginal = data.message.split("|")[1];
-                    if (confirm(`Você está tentando se apresentar em outra supervisão. Sua supervisão original é: ${supervisaoOriginal}.\nDeseja continuar?`)) {
-                        formData.append("confirmado", "1");
-    
-                        // Reenvia com confirmação
-                        return await postApresentacao(formData);
-                    }
-                } else {
-                    alert("Erro ao registrar: " + data.message);
-                }
-            }
-    
+            //console.log("Resposta da API:", data);
+            
             return data;
         } catch (error) {
             console.error("Falha ao enviar dados da apresentação:", error);
@@ -68,5 +55,77 @@ export const api = {
             return { success: false, message: error.message };
         }
     },
-    // Parte da Prontidão
+    /**
+     * 
+     * @param {FormData} 
+     * @returns {Promise<object>}
+     */
+    postProntidao: async (formData) => {
+        try {
+            const url = `${API_BASE_URL}post_prontidao.asp`;
+
+            const response = await fetch(url, {
+                method: 'POST',
+                body: formData,
+            });
+
+            if (!response) {
+                throw new Error(`Error HTTP: ${response.status}`);
+            }
+
+            const dados = await response.json();
+            return dados;
+        } catch (error) {
+            console.error("Falha ao enviar prontidão:", error);
+            alert("Erro na requisição de prontidão: " + error.message);
+            return { success: false, message: error.message };
+        }
+    },
+    /**
+     * 
+     * @param {FormData} 
+     * @returns {Promise<object>}
+     */
+    postJustificativaApresentacao: async (formData) => {
+        try {
+            const url = `${API_BASE_URL}post_update_just_apresentacao.asp`;
+
+            const response = await fetch(url, {
+                method: 'POST',
+                body: formData,
+            });
+
+            const dados = await response.json();
+            return dados;
+        } catch (error) {
+            console.error("Falha ao enviar justificativa:", error);
+            alert("Erro na requisição de justificativa: " + error.message);
+            return { success: false, message: error.message };
+        }
+    },
+    /**
+     * @param {FormData}
+     * @returns {Promise<object>}
+     */
+    postFimJornada: async (formData) => {
+        try {
+            const url = `${API_BASE_URL}post_fim_jornada.asp`;
+
+            for (let [key, value] of formData.entries()) {
+                console.log(`FormData - ${key}: ${value}`);
+            }
+
+            const response = await fetch(url, {
+                method: 'POST',
+                body: formData,
+            });
+
+            const dados = await response.json();
+            return dados;
+        } catch (error) {
+            console.error("Falha ao enviar dados Fim de Jornada: ", error);
+            alert("Erro na requisição de Fim de Jornada: ", error.message);
+            return { success: false, message: error.message };
+        }
+    },
 }
