760f6e305d61697e04c7b5c8cbc78e9ef5e7e46f
Enzo Spadetto Rufino <127460831+zinh9@users.noreply.github.com>
2025-11-10 21:20:58 -0300

    Implement clock display functionality
    Added a function to start a clock that updates every second.

--- v2/js/utils/helpers.js
+++ v2/js/utils/helpers.js
@@ -3,3 +3,19 @@ export function horaParaMinutos(horaStr) {
     const [h, m] = horaStr.split(':').map(Number);
     return (h || 0) * 60 + (m || 0);
 }
+
+export function iniciarRelogio() {
+    setInterval(() => {
+        const d = new Date();
+        let displayDate;
+        if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+            displayDate = d.toLocaleTimeString('pt-BR');
+        } else {
+            displayDate = d.toLocaleTimeString('pt-BR', {timeZone: 'America/Belem'});
+        }
+        const clockEl = document.getElementById('clock-display');
+        if (clockEl) {
+            clockEl.innerHTML = displayDate;
+        }
+    }, 1000);
+}
