ffe38fa359567ec800002493506d5fcd291db0d3
Enzo Spadetto Rufino <127460831+zinh9@users.noreply.github.com>
2025-11-11 18:41:34 -0300

    Refactor import paths and enhance layout handling

--- v2/js/main.js
+++ v2/js/main.js
@@ -1,19 +1,43 @@
-import { viewLocal } from './views/localView.js'
+import { viewLocal } from './views/viewLocal.js'
+import { viewCCP } from './views/viewCCP.js';
 
 function app() {
     const params = new URLSearchParams(window.location.search);
     const supervisao = params.get('sup');
     const local = params.get('loc');
     const layout = params.get('layout');
+
+    setHeaderLayout(layout);
     
     if (layout === 'patio' && supervisao && local) {
         viewLocal.init(supervisao, local);
+    } else if (layout === 'CCP') {
+        viewCCP.init(supervisao);
     } else {
         document.getElementById('page-header').innerHTML = `
             <h1 class="text-danger">Erro: Supervisão ou Local não especificado.</h1>
             <p class="text-white">Por favor, acesse usando uma URL válida!</p>
         `;
-    } // Fazer para o CCP a partir da Query String 'layout'
+    }
+}
+
+function setHeaderLayout(layoutParam) {
+    const layoutLabelEl = document.getElementById('layout-label');
+    if (!layoutLabelEl) return;
+
+    const layout = (layoutParam || '').toString().trim().toLowerCase();
+
+    if (layout === 'ccp') {
+        document.title = 'CCP | Controle de Apresentação';
+        layoutLabelEl.textContent = 'CCP';
+        layoutLabelEl.className = "text-danger";
+    } else if (layout === 'patio') {
+        document.title = 'PÁTIO | Controle de Apresentação';
+        layoutLabelEl.textContent = 'PÁTIO';
+    } else {
+        document.title = 'PÁTIO | Controle de Apresentação';
+        layoutLabelEl.textContent = 'PÁTIO';
+    }
 }
 
 app();
