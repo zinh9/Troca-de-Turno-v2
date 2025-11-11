55333e3256107aa2fb43a70ddbdd28424fcd0387
Enzo Spadetto Rufino <127460831+zinh9@users.noreply.github.com>
2025-11-10 21:21:17 -0300

    Refactor app function to use viewLocal
    Updated the import and condition to use viewLocal based on layout parameter.

--- v2/js/main.js
+++ v2/js/main.js
@@ -1,18 +1,19 @@
-import { localView } from './views/localView.js'
+import { viewLocal } from './views/localView.js'
 
 function app() {
     const params = new URLSearchParams(window.location.search);
     const supervisao = params.get('sup');
     const local = params.get('loc');
+    const layout = params.get('layout');
     
-    if (supervisao && local) {
-        localView.init(supervisao, local);
+    if (layout === 'patio' && supervisao && local) {
+        viewLocal.init(supervisao, local);
     } else {
         document.getElementById('page-header').innerHTML = `
             <h1 class="text-danger">Erro: Supervisão ou Local não especificado.</h1>
             <p class="text-white">Por favor, acesse usando uma URL válida!</p>
         `;
-    }
+    } // Fazer para o CCP a partir da Query String 'layout'
 }
 
 app();
